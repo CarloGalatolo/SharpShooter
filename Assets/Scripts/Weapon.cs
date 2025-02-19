6c4feb1b525805737c5cbd6834b35ec8cb1d1830
ALIENWARE-2\Carlo <carlogalatolo@gmail.com>
2025-02-19 11:57:55 +0100

    Fino a lezione 162

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
 	[SerializeField] ParticleSystem muzzleFlash;
+	[SerializeField] GameObject hitVFXPrefab;
 	[SerializeField] uint damageAmount = 1;
 
 	StarterAssetsInputs starterAssetsInputs;
+	Animator animator;
+
+	const string SHOOT_ANIMATION = "Shoot";
 
 
 	void Awake()
 	{
 		starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+		animator = GetComponentInParent<Animator>();
 	}
 
 
@@ -24,15 +29,16 @@ public class Weapon : MonoBehaviour
 	private void HandleShoot ()
 	{
 		if (!starterAssetsInputs.shoot) return;
+		starterAssetsInputs.ShootInput(false);	// Reset input value.
 		
 		muzzleFlash.Play();
+		animator.Play(SHOOT_ANIMATION, 0, 0);
 
-		if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, Mathf.Infinity))
+		if ( Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, Mathf.Infinity) )
 		{
-			EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-			enemyHealth?.TakeDamage(damageAmount);
+			Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);	// Shot hit VFX;
+			EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();	// Check if hit an enemy.
+			enemyHealth?.TakeDamage(damageAmount);	// Damage enemy if hit.
 		}
-
-		starterAssetsInputs.ShootInput(false);
 	}
 }
